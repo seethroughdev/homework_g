13b87f8d5e6e2f64840fe8d1d37bac20cafe30bc
adam <seethroughdev@gmail.com>
2021-08-18 23:02:04 -0700

    add styles for dog column

--- src/App.js
+++ src/App.js
@@ -34,16 +34,12 @@ function App() {
       <div className="bg-dark p-5 rounded-3 mb-5">
         <h1 className="display-1 text-white">Infinite Dogs!</h1>
       </div>
-      <h3>{dogsFetchStatus}</h3>
-      <ul>
-        {dogs.map((d, i) => {
-          const isLast = i === dogs.length - 1;
-          return (
-            <li key={d} ref={isLast ? lastDogRef : undefined}>
-              <img src={d} alt="dog" />
-            </li>
-          );
-        })}
+      <ul className="dogs-list">
+        {dogs.map((d, i) => (
+          <li key={d} ref={i === dogs.length - 1 ? lastDogRef : undefined}>
+            <img src={d} alt="dog" />
+          </li>
+        ))}
       </ul>
     </Container>
   );
